8d2f522c0b39a9fcf3908fd381bc5f248a505fd7
GreenCobalt <34927146+GreenCobalt@users.noreply.github.com>
2025-01-06 01:30:00 -0500

    add logging to file

--- crawlers/utils/logger.py
+++ crawlers/utils/logger.py
@@ -1,5 +1,11 @@
 import logging
 
+logging.basicConfig(filename='/var/log/tiktokapi.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
 def log_setup(log_to_console=True):
     logger = logging.getLogger("TiktokAPI")
     return logger
